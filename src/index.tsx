d382ce20439a56d6cdddfe48a2de062f5770a103
Ademir__Dev <gaparov866@gmail.com>
2023-04-17 14:50:39 +0600

    oooo

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,22 @@ import './index.scss';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import Layout from "./layout/Layout";
-
+import {setUpStore} from "./Store/store";
+import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
+const store = setUpStore()
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <Layout>
-              <App />
-          </Layout>
-      </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Layout>
+                    <App/>
+                </Layout>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 )
